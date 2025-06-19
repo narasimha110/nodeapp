5cfdc942408840d6ddbe6d943f61caaaf43cded6
narasimha110 <141666881+narasimha110@users.noreply.github.com>
2025-06-19 11:33:48 +0530

    Update Edit.jsx

--- nodeapp/src/elements/Edit.jsx
+++ nodeapp/src/elements/Edit.jsx
@@ -7,7 +7,7 @@ function Edit() {
   const { id } = useParams();
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/get_student/${id}`)
+      .get(`http://3.108.249.118:5000/get_student/${id}`)
       .then((res) => {
         setData(res.data);
       })
